4a7f461ef3002de18ec658ade6947b70c4e512d5
carosaut <102183897+carosaut@users.noreply.github.com>
2023-04-20 09:45:21 +0100

    Create avatar_urls.js

---
+++ avatar_urls.js
@@ -0,0 +1,6 @@
+var urls = [
+  "https://cdn.midjourney.com/7d03081b-0eaf-4497-a94b-494184b574c4/grid_0.png",
+  "https://cdn.midjourney.com/6977701c-7af1-4ce3-9324-b1fd5d3d2868/grid_0.png",
+  "https://cdn.midjourney.com/634bfbce-c830-43ba-a16c-4c8b5c6133b5/grid_0.png",
+  "https://cdn.midjourney.com/20ad32df-7afb-4387-940a-9b859eb1f354/grid_0.png"
+];
